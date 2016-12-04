945fcdcbf6c30abe441f8a1131824925f50fd64e
Mario Nachbaur <marionauta@users.noreply.github.com>
2016-12-04 12:34:53 +0100

    Use a buffered reader for stdin. (#1)
    - Use `std::io::BufRead` to read `std::io::stdin()`.
    - Handle the logic in the iterator.
    
    This should be faster and use way less memory.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{self, Read};
+use std::io::{self, BufRead};
 
 fn digits_sum(n: u64, acc: u64) -> u64 {
     match n {
@@ -7,33 +7,17 @@ fn digits_sum(n: u64, acc: u64) -> u64 {
     }
 }
 
-fn harshad(n: u64) -> bool {
-    n % digits_sum(n, 0) == 0
-}
-
-fn read_stdin() -> Result<String, io::Error> {
-    let mut res = String::new();
-
-    match io::stdin().read_to_string(&mut res) {
-        Ok(_) => Ok(res),
-        Err(e) => Err(e),
-    }
-}
-
 fn main() {
-    let numbers: Vec<u64> = match read_stdin() {
-        Ok(content) => content.split_whitespace()
-            .map(str::parse)
-            .filter(Result::is_ok)
-            .map(Result::unwrap)
-            .collect(),
-        Err(_) => Vec::new(),
-    };
-            
+    let stdin = io::stdin();
+    let numbers = stdin.lock().lines()
+        .filter(Result::is_ok)
+        .map(|rs| rs.unwrap().parse())
+        .filter(Result::is_ok)
+        .map(Result::unwrap)
+        .filter(|&n| n % digits_sum(n, 0) == 0)
+        .collect::<Vec<u64>>();
 
     for n in numbers {
-        if harshad(n) {
-            println!("{}", n);
-        }
+        println!("{}", n);
     }
 }
